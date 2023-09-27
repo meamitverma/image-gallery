d6397e40e5789654cbbc5d87634b0c7ac0cc4d41
Amit Verma <amit.verma.22080@gmail.com>
2023-09-27 16:46:56 +0530

    image full height can be seen now

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <header className="bg-black sticky top-0 z-10">
       <nav className="flex flex-col gap-4 sm:flex-row sm:justify-between items-center p-4 font-bold max-w-6xl mx-auto text-white">
         <h1 className="text-2xl sm:text-3xl text-center whitespace-nowrap">
-          <Link href="/">Next.js Image Gallery</Link>
+          <Link href="/">LostPicassa. Image Gallery</Link>
         </h1>
         <Search />
       </nav>
